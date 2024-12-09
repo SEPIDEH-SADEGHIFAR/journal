4f256ad5bfa8a49e0d29d94ef151770d144dc5fa
seyedeh sepideh sadeghi far <sepideh.sadeghifar12@gmail.com>
2024-12-09 12:13:26 +0100

    beggining

---
+++ juornal/juornal/juornalApp.swift
@@ -0,0 +1,17 @@
+//
+//  juornalApp.swift
+//  juornal
+//
+//  Created by seyedeh sepideh sadeghi far on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct juornalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
